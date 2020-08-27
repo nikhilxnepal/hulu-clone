e262a7d813596e3ef3468d05cca8b9d187c8bb99
Nikhil Nepal <nikhilnepal@users.noreply.github.com>
2020-08-27 11:55:53 +0545

    first commit

---
+++ src/Results.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import "./Results.css";
+import VideoCard from "./VideoCard";
+import axios from "./axios";
+import FlipMove from "react-flip-move";
+
+function Results({ selectedOption }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    //Run this code once when the Results component loads/mounts
+    async function fetchData() {
+      const request = await axios.get(selectedOption);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [selectedOption]);
+  return (
+    <div className="results">
+      <FlipMove>
+        {movies.map((movie) => (
+          <VideoCard key={movie.id} movie={movie} />
+        ))}
+      </FlipMove>
+    </div>
+  );
+}
+
+export default Results;
